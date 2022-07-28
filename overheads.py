d01a403c742c301b29e8d4e3191d8b30054f7a45
NgeeAnnAccountancy <s10239641@connect.np.edu.sg>
2022-07-28 18:01:50 +0800

    creating a variable file_path to access the Overheads.CSV file.

--- overheads.py
+++ overheads.py
@@ -1,25 +1,5 @@
 from pathlib import Path
 import csv
-from re import T
 
 file_path = Path.cwd()/"csv_report"/"Overheads.csv"
-
-list = []
-
-def overheads():
-    with file_path.open(mode = "r", encoding = "UTF-8") as data:
-        reader = csv.reader(data)
-        next(reader)
-
-        for file in reader:
-            list.append(file[1])
-            list.sort(key = float, reverse= True)
-        print(list)
-        # if list == reader("[Overheads]"):
-        #     return reader
-
-print(overheads())
-        
-
-
 
