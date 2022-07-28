e1d006f3d53cb146e72964394ec14b8e274649d2
NgeeAnnAccountancy <s10239641@connect.np.edu.sg>
2022-07-28 18:08:45 +0800

    Creating a function "Overheads".

--- overheads.py
+++ overheads.py
@@ -3,3 +3,18 @@ import csv
 
 file_path = Path.cwd()/"csv_report"/"Overheads.csv"
 
+list = []
+
+def Overheads():
+    with file_path.open(mode = "r", encoding = "UTF-8") as data:
+        reader = csv.reader(data)
+        next(reader)
+
+        for file in reader:
+            list.append(file[1])
+            list.sort(key= float, reverse = True)
+
+        return list
+
+
+print(Overheads())
