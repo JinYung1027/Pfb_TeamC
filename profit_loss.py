570a03a949ca45648b178052656d36fe9681a394
“BryanWee05” <S10243500@connect.np.edu.sg>
2022-07-28 12:27:45 +0800

    abcd

---
+++ profit_loss.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+import csv
+
+print(Path.cwd())
+Path = Path.cwd()/"csv_report"/"Profit and Loss.csv"
+print(Path.exists())
+list = []
+
+def profitloss():
+
+    with Path.open(mode = "r", encoding = "UTF-8", newline= "") as Path1:
+        reader = csv.reader(Path1)
+        next(reader)
+
+        for line in reader:
+            list.append(line[4])
+        return list
+
+
+print(profitloss())
