1913bc2387a6ee95ca32ed7ee102eda4a11e5ad2
NgeeAnnAccountancy <110024384+NgeeAnnAccountancy@users.noreply.github.com>
2022-07-28 13:54:48 +0800

    Add files via upload

---
+++ overheads.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+import csv
+from re import T
+
+file_path = Path.cwd()/"csv_report"/"Overheads.csv"
+
+list = []
+
+def overheads():
+    with file_path.open(mode = "r", encoding = "UTF-8") as data:
+        reader = csv.reader(data)
+        next(reader)
+
+        for file in reader:
+            list.append(file[1])
+            list.sort(key = float, reverse= True)
+        return list
+        # if list == reader("[Overheads]"):
+        #     return reader
+
+print(overheads())
+        
+
+
+
