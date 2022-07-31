b0c78612bf4b2985495976210e052bc8af695d96
“BryanWee05” <S10243500@connect.np.edu.sg>
2022-07-31 17:56:43 +0800

    h

--- try.py
+++ try.py
@@ -1,20 +1,3 @@
-from pathlib import Path
-import csv
+import profit_loss
 
-file_path = Path.cwd()/"csv_report"/"Overheads.csv"
-
-list = []
-list2= []
-def Overheads():
-    with file_path.open(mode = "r", encoding = "UTF-8") as data:
-        reader = csv.reader(data)
-        next(reader)
-        for cost,data in reader:
-            data=float(data)
-            a=f"{data},{cost}"
-            list.append(a)
-            list.sort(reverse=True)
-            highest_overhead=list[0]
-            return(highest_overhead)
-
-print(Overheads())
+print(profitloss())
