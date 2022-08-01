81a4a43b6b6e61130edbae8b4c356490341ab3ac
JinYung1027 <110020036+JinYung1027@users.noreply.github.com>
2022-08-01 16:04:04 +0800

    qwer

--- try.py
+++ try.py
@@ -1,3 +1,20 @@
-import profit_loss
+from pathlib import Path
+import csv,re,api
 
-print(profit_loss.profitloss())
+file_path = Path.cwd()/"csv_report"/"Overheads.csv"
+
+list = []
+list2= []
+def Overheads():
+    with file_path.open(mode = "r", encoding = "UTF-8") as data:
+        reader = csv.reader(data)
+        next(reader)
+        for cost,data in reader:
+            data=float(api.exc(data))
+            a=f"{data},{cost}"
+            list.append(a)
+            list.sort(reverse=True)
+            highest_overhead=list[0]
+            return(highest_overhead())
+
+print(Overheads())
