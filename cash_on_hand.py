2d57dbb5163acbb0785c3129287b1740007fcf22
13954x <13954x@gmail.com>
2022-08-03 16:14:24 +0800

    asd

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,19 +1,55 @@
 from pathlib import Path
 import re, csv
 
-cwd = Path.cwd()/'project_group'
-csv_filepath = Path.cwd()/'csv_report'/'Cash on Hand.csv'
+csv_filepath = Path.cwd()/'csv_report'
 
-def coh():    
-    with csv_filepath.open(mode = 'r' , encoding= 'UTF-8') as file:
 
-        reader=csv.reader(file)
+for csvdata in csv_filepath.glob('Cash on Hand.csv'):
         
-        for text in reader:
-            print(text)
+    with csvdata.open(mode = 'r' , encoding= 'UTF-8') as file:
 
-coh()
+        x = 0
 
+        data = file.read()
+
+        datas = []
+
+        cash = re.findall(pattern=r'[0-9][0-9][0-9]+' , string=data)
+
+        cashsort = re.findall(pattern=r'[0-9][0-9][0-9]+' , string=data)
+
+        cashsort.sort()
+
+        startday = re.search(pattern=r'[0-9][0-9]' , string=data)
+
+        startday = startday.group()
+
+        if cash == cashsort:
+            
+            print('[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY')
+
+        else:
+            
+            
+
+            for x in range(0,5):
+
+                if float(cash[x+1]) < float(cash[x]) :
+                    
+                    deficit = float(cash[x]) - float(cash[x+1])
+
+                
+
+                    print(f'[CASH DEFICIT] DAY: {float(startday) + x + 1} , AMOUNT: SGD{deficit}')
+
+                    
+
+                
+                x = x + 1
+
+                
+
+                
 
 
     
