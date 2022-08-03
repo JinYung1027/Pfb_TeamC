028cb5bd56dca24097a8d6cc991f11455bfa582e
JinYung1027 <110020036+JinYung1027@users.noreply.github.com>
2022-08-03 16:20:23 +0800

    sadfasfdasdf

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import api, cash_on_hand, overheads, profit_loss, csv
+import api, profit_loss, csv
 from pathlib import Path
 
 fp = Path.cwd()/"summary_report.csv"
@@ -7,4 +7,4 @@ fp.touch()
 with fp.open(mode="w",encoding="UTF-8",newline="") as file:
     writer=csv.writer(file)
     writer.writerow([f"[REAL TIME CURRENCY CONVERSION RATE] USD1 = SGD{api.Exchange_rate}"])
-    writer.writerow([profit_loss.profitloss()])
+    writer.writerows([profit_loss.profitloss()])
