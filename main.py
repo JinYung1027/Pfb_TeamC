5ecf877ea1c30c17cc5330ee1371d5109c1f5ecc
JinYung1027 <110020036+JinYung1027@users.noreply.github.com>
2022-08-04 13:44:50 +0800

    fgsdghdfhfdg

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-# Import module and functions needed for writing the csv file
+# Import module and functions needed for writing the text file
 import api, overheads, profit_loss,cash_on_hand, csv
 from pathlib import Path
 
-
+# Create a function to write the data into text file
 def main():
 
-# Create a file path to the new file named "summary_report.csv" in the current working directory.
+# Create a file path to the new file named "summary_report.txt" in the current working directory.
     fp = Path.cwd()/"summary_report.txt"
 
-# Create the file "summary_report.csv" in current working directory using .touch() method
+# Create the file "summary_report.txt" in current working directory using .touch() method
     fp.touch()
 
 # Open the csv file in write mode by using the .open() method
     with fp.open(mode="w") as file:
-        
+
+        # .write() method writes data to a plain text file
+        ## added a "\n" to create a new line before the next data
         file.write(f"[REAL TIME CURRENCY CONVERSION RATE] USD1 = SGD{api.Exchange_rate}" + "\n")
         file.write(overheads.Overheads() + "\n")
     # Use for loop to write the data that might have one or more outcome
@@ -22,5 +24,5 @@ def main():
         for rows in profit_loss.profitloss():
             file.write(f"{rows}" + "\n")
 
-
+# Execute the function
 main()
